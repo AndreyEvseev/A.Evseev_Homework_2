da7c832908f56b0631799f330d9299572dbe0ae1
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 17:05:02 +0300

    добавлен код по заданию 2 семинара 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа
+
+int number3, digit1, digit3;
+number3 = new Random().Next(100, 1000);
+Console.Write(number3 + " -> ");
+digit1 = number3 / 100;
+digit3 = number3 % 10;
+Console.WriteLine($"{digit1 * 10 + digit3};");
