6da710535b7db04febf094a81013da5071086c61
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 18:45:31 +0300

    добавлен код по заданию 3 семинара 2

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+
+int number = 12456;
+int i = 1, count = 0, div = 1;
+int help = 0;
+while(number / i > 0)
+{
+    i *= 10;
+    count++ ;
+}
+if(count < 3) Console.WriteLine(number + " -> третьей цифры нет");
+else
+    {   if(count > 3)
+        {
+            i = 1;
+            while(i < count - 2)
+            {
+                div = div * 10;
+                i++;
+            }
+        }
+    help = number / div;
+    Console.WriteLine($"{number} -> {help % 10}");
+}
