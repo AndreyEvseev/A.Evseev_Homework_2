b53be9451e748420e1edf014cda4120dea9022a1
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 16:49:07 +0300

    добавлен код по заданию 1 семинара 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа
+
+int number3 = 918;
+if(number3 > 99 && number3 < 1000) Console.WriteLine(number3 / 10 % 10 );
+else Console.WriteLine("Ошибка! Число не явлется трёхзначным");
