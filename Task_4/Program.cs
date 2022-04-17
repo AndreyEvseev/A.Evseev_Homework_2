1f575c0cf012201a0b325d8291d7d464fe30d475
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 19:12:15 +0300

    добавлен код по заданию 4 семинара 2

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным
+
+int nomDayWeek = 7;
+
+if(nomDayWeek > 7 || nomDayWeek < 1) Console.WriteLine("Ошибка! Задан некорректный номер дня недели");
+else
+{
+    if(nomDayWeek == 6 || nomDayWeek == 7) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+
